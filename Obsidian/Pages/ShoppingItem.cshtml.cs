6ea04f286d1324225cb66b6a06e3430c7af0e16f
duverneuil-samuel <samuel.duverneuil.pro@gmail.com>
2025-09-05 12:44:33 +0200

    teste session

--- Obsidian/Pages/ShoppingItem.cshtml.cs
+++ Obsidian/Pages/ShoppingItem.cshtml.cs
@@ -9,16 +9,19 @@ public class ShoppingModel : PageModel
 
     public void OnGet()
     {
-         Panier = HttpContext.Session.GetObject<List<ShoppingItem>>("Panier") ?? new List<ShoppingItem>();
-
+        // Panier = HttpContext.Session.GetObject<List<ShoppingItem>>("Panier") ?? new List<ShoppingItem>();
+        Panier = HttpContext.Session.GetObject<List<ShoppingItem>>("Panier");
 
         // Exemple a remplacé par bdd
-        if (TempData["Panier"] is List<ShoppingItem> tempPanier)
+        // if (TempData["Panier"] is List<ShoppingItem> tempPanier)
+        if (Panier == null || !Panier.Any())
         {
-            Panier = tempPanier;
-        }
-        else
-        {
+            // Panier = tempPanier;
+            
+            
+        // }
+        // else
+        // {
             // ✅ Simulation de données factices
             Panier = new List<ShoppingItem>
         {
@@ -26,6 +29,7 @@ public class ShoppingModel : PageModel
             new ShoppingItem { ProductName = "Souris gaming", Seller = "Lucie", Condition = "Occasion", Language = "EN", Price = 49.50m, Quantity = 2 },
             new ShoppingItem { ProductName = "Tapis de souris XXL", Seller = "Jean", Condition = "Neuf", Language = "FR", Price = 25m, Quantity = 1 }
         };
+            HttpContext.Session.SetObject("Panier", Panier);
         }
     }
 
