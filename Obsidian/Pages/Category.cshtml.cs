2b0a6aa82b67bef8b4223e50a1133c222f095bc8
duverneuil-samuel <samuel.duverneuil.pro@gmail.com>
2025-09-10 16:07:02 +0200

    modif pour bdd

--- Obsidian/Pages/Category.cshtml.cs
+++ Obsidian/Pages/Category.cshtml.cs
@@ -1,6 +1,12 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Obsidian.Models; 
 
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Obsidian.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
 namespace Obsidian.Pages
 {
     public class CategoryModel : PageModel
@@ -13,27 +19,45 @@ namespace Obsidian.Pages
         }
         public string CategoryName { get; set; }
         public string CategoryDescription { get; set; }
-        public List<Product> Products { get; set; }
+        public List<Product> Products { get; set; } = new List<Product>();
         public int CategoryId { get; set; }
 
-        public void OnGet(int? categoryId)
+        // public void OnGet(int? categoryId)
+        // {
+        //     if (categoryId.HasValue)
+        //     {
+        //         CategoryId = categoryId.Value;
+
+        //         var category = _context.Categories
+        //                                .FirstOrDefault(c => c.Id == CategoryId);
+
+        //         if (category != null)
+        //         {
+        //             CategoryName = category.Name;
+        //             CategoryDescription = category.Description;
+
+        //             Products = _context.Products
+        //                                .Where(p => p.CategoryId == CategoryId)
+        //                                .ToList();
+        //         }
+        //     }
+        // }
+           public async Task OnGetAsync(string name)
         {
-            if (categoryId.HasValue)
+            if (string.IsNullOrEmpty(name))
+                return;
+
+            var category = await _context.Categories
+                                         .FirstOrDefaultAsync(c => c.Name.ToLower() == name.ToLower());
+
+            if (category != null)
             {
-                CategoryId = categoryId.Value;
+                CategoryName = category.Name;
+                CategoryDescription = category.Description;
 
-                var category = _context.Categories
-                                       .FirstOrDefault(c => c.Id == CategoryId);
-
-                if (category != null)
-                {
-                    CategoryName = category.Name;
-                    CategoryDescription = category.Description;
-
-                    Products = _context.Products
-                                       .Where(p => p.CategoryId == CategoryId)
-                                       .ToList();
-                }
+                Products = await _context.Products
+                                         .Where(p => p.CategoryId == category.Id)
+                                         .ToListAsync();
             }
         }
     }
